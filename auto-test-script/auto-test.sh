2ae0394baa4090839a264387b994d27d54cf5770
yu-hsieh <Yu-cheng.Hsieh@amd.com>
2022-07-24 20:46:53 -0600

    add more type to auto-test

--- auto-test-script/auto-test.sh
+++ auto-test-script/auto-test.sh
@@ -5,6 +5,7 @@ for alpha in 1 2
 do
     for beta in 0 1
     do
+#        for ty in "s" "c" "d" "z"
         for ty in "h" "hh" "hb" "i8"
         do
             for tranA in "True" "False"
@@ -15,7 +16,19 @@ do
                     echo "  DataInitTypeAlpha: $alpha" >> t.yaml
                     echo "  DataInitTypeBeta: $beta" >> t.yaml
                     cat t.yaml middle.yaml > tt.yaml
-                    if [ $ty == "h" ]
+                    if [ $ty == "s" ]
+                    then
+                        echo "      DataType: s" >> tt.yaml
+                    elif [ $ty == "d" ]
+                    then
+                        echo "      DataType: d" >> tt.yaml
+                    elif [ $ty == "c" ]
+                    then
+                        echo "      DataType: c" >> tt.yaml
+                    elif [ $ty == "z" ]
+                    then
+                        echo "      DataType: z" >> tt.yaml
+                    elif [ $ty == "h" ]
                     then
                         echo "      DataType: h" >> tt.yaml
                     elif [ $ty == "hh" ]
@@ -45,9 +58,11 @@ do
         done
     done
 done
-echo "egrep FAIL"
-egrep -srn FAIL log
-echo "egrep success"
+echo "Total log"
+ls -l log/*.log | wc -l
+echo "num of success"
 egrep -srn "clientExit=0" log | wc -l
-echo "egrep fail"
+echo "num of failure"
+egrep -srn "clientExit=1" log | wc -l
+echo "fail items"
 egrep -srn "clientExit=1" log
